a2085b0d5e202d264f1cb23b1d93e1d3a429b284
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-17 02:15:08 +0900

    CREATE; default config file

---
+++ src/configs/index.ts
@@ -0,0 +1,19 @@
+import databaseConfigList from "./database-config";
+
+type NodeEnvironment = "production" | "test" | "development";
+
+const env: NodeEnvironment = process.env.NODE_ENV as NodeEnvironment;
+const databaseConfig = databaseConfigList[env];
+
+export const config = {
+  NodeEnv: env,
+  type: databaseConfig.type,
+  dbHost: databaseConfig.host,
+  dbPort: databaseConfig.port,
+  dbUser: databaseConfig.username,
+  dbPassword: databaseConfig.password,
+  dbName: databaseConfig.database,
+  dbSynchronize: databaseConfig.synchronize,
+  dbLogging: databaseConfig.logging,
+  jwtSecret: process.env.JWT_SECRET,
+} 
