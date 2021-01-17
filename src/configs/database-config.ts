09840d41d954eeba02cb3ddd6adc63c5f121a2ce
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-17 20:52:41 +0900

    UPDATE; database config file
    synchronize setting

--- src/configs/database-config.ts
+++ src/configs/database-config.ts
@@ -11,7 +11,7 @@ const databaseConfigList = {
     username: process.env.DEVELOPMENT_USERNAME,
     password: process.env.DEVELOPMENT_PASSWORD,
     database: process.env.DEVELOPMENT_DATABASE,
-    synchronize: false,
+    synchronize: true,
     logging: true,
   },
   test: {
@@ -31,8 +31,8 @@ const databaseConfigList = {
     username: process.env.PRODUCTION_USERNAME,
     password: process.env.PRODUCTION_PASSWORD,
     database: process.env.PRODUCTION_DATABASE,
-    synchronize: true,
-    logging: false,
+    synchronize: false,
+    logging: true,
   }
 }
 
