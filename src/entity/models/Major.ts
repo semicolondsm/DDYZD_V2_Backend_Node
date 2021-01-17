00699a24311b0a7a6ece650be69abe3e57dcf31d
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-17 16:30:27 +0900

    CREAET; major model

---
+++ src/entity/models/Major.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Club } from "./Club";
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+
+@Entity("major")
+export class Major extends EntityWithIdColumn {
+  @Column({ type: "varchar", length: 45 })
+  majorname: string;
+
+  @ManyToOne(() => Club, club => club.majors) 
+  @JoinColumn({ name: "club_id" })
+  club: Club;
+}
