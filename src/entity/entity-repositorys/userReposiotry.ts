a4f2444c08a6f56cf7bf99bd08eb2f8b32235c4f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-18 00:20:25 +0900

    CREATE; user repository
    add find user by class number

---
+++ src/entity/entity-repositorys/userReposiotry.ts
@@ -0,0 +1,17 @@
+import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { User } from "../models";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  
+  static getQueryRepository() {
+    return getCustomRepository(UserRepository);
+  }
+
+  public findUserByClassIdentity(gcn: string): Promise<User> {
+    return this.createQueryBuilder()
+    .where("user.gcn = :gcn")
+    .setParameter("gcn", gcn)
+    .getOne();
+  }
+}
