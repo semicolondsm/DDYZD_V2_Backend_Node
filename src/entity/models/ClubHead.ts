5c4e4c208c0e68dec2708e4e40a2875c5503dbd6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-17 21:13:57 +0900

    CREATE; club head model

---
+++ src/entity/models/ClubHead.ts
@@ -0,0 +1,19 @@
+import { Entity, JoinColumn, ManyToOne, OneToMany, OneToOne } from "typeorm";
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+import { Club } from './Club';
+import { User } from "./User";
+import { Room } from "./Room";
+
+@Entity("club_head")
+export class ClubHead extends EntityWithIdColumn {
+  @OneToOne(() => Club, club => club.head)
+  @JoinColumn({ name: "club_id" })
+  club: Club;
+
+  @ManyToOne(() => User, user => user.heads)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @OneToMany(() => Room, room => room.clubHead)
+  rooms: Room[];
+}
