238f41d933d16e8d5d171c555694b96d609ba473
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-17 22:31:54 +0900

    CREATE; chat model

---
+++ src/entity/models/Chat.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+import { Room } from "./Room";
+
+@Entity("chat")
+export class Chat extends EntityWithIdColumn {
+  @Column({ type: "varchar", length: 512 })
+  msg: string;
+
+  @CreateDateColumn({ type: "datetime" })
+  created_at: Date;
+
+  @Column({ type: "enum", nullable: true, enum: ["U", "S", "H"]})
+  user_type: "U" | "S" | "H";
+
+  @ManyToOne(() => Room, room => room.chats)
+  @JoinColumn({ name: "room_id" })
+  room: Room;
+}
