268ec65b8dd269bf5e244f3b80f89c729456ce63
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-17 20:31:33 +0900

    CREATE; tag model

---
+++ src/entity/models/Tag.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, JoinTable, ManyToMany, OneToMany } from "typeorm";
+import { ClubHasTag } from "./ClubHasTag";
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+
+@Entity("tag") 
+export class Tag extends EntityWithIdColumn {
+  @Column({ type: "varchar", length: 45 })
+  title: string;
+  
+  @OneToMany(() => ClubHasTag, clubHasTag => clubHasTag.tag)
+  clubHasTags: ClubHasTag[];
+}
