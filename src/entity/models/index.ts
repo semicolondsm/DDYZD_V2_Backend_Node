66a09cda4de19a19f3291e8efa66280f9e72a458
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-18 00:21:11 +0900

    CREATE; model index file
    model modularization

---
+++ src/entity/models/index.ts
@@ -0,0 +1,35 @@
+import { Alarm } from "./Alarm";
+import { Application } from "./Application";
+import { Chat } from "./Chat";
+import { Club } from "./Club";
+import { ClubFollow } from './ClubFollow';
+import { ClubHasTag } from './ClubHasTag';
+import { ClubHead } from "./ClubHead";
+import { Feed } from "./Feed";
+import { FeedFlag } from './FeedFlag';
+import { FeedMedium } from './FeedMedium';
+import { Major } from "./Major";
+import { Option } from "./Option";
+import { Room } from "./Room";
+import { Supply } from "./Supply";
+import { Tag } from "./Tag";
+import { User } from "./User";
+
+export {
+  Alarm,
+  Application,
+  Chat,
+  Club,
+  ClubFollow,
+  ClubHasTag,
+  ClubHead,
+  Feed,
+  FeedFlag,
+  FeedMedium,
+  Major,
+  Option,
+  Room,
+  Supply,
+  Tag,
+  User
+}
