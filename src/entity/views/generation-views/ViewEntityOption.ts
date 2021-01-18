58e37f2f693a9208783e3f29e1d208c8bed29217
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-19 06:24:54 +0900

    CREATE; view entity option

---
+++ src/entity/views/generation-views/ViewEntityOption.ts
@@ -0,0 +1,12 @@
+import fs from "fs";
+import path from "path";
+
+export class CustomViewEntityOption {
+  public readonly expression: string;
+  public readonly name: string;
+
+  constructor(viweName: string, queryPath: string) {
+    this.expression = fs.readFileSync(path.join(__dirname, `./querys/${queryPath}`), { encoding: "utf8" });
+    this.name = viweName;
+  }
+}
