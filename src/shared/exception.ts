79e3c70eb14d83f9accf6ad4dd1b5945848db40c
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-21 04:28:26 +0900

    UPDATE; add exceptions

--- src/shared/exception.ts
+++ src/shared/exception.ts
@@ -9,8 +9,38 @@ export class HttpError extends Error {
   }
 }
 
+export class UnAuthorizedTokenError extends HttpError {
+  constructor() {
+    super(401, "Unauthorized Token");
+  }
+}
+
+export class ExpiredTokenError extends HttpError {
+  constructor() {
+    super(401, "Expired Token");
+  }
+}
+
+export class ForbiddenError extends HttpError {
+  constructor() {
+    super(403, "Forbidden Request");
+  }
+}
+
 export class NotFoundError extends HttpError {
   constructor() {
     super(404, "Not Found");
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor() {
+    super(400, "Bad Request");
+  }
+}
+
+export class InternalServerError extends HttpError {
+  constructor() {
+    super(500, "Interal Server Error");
   }
 }
