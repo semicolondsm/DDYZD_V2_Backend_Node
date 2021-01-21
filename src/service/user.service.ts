c6f50ff61a1d835551b2293612b1b04a483e2496
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-21 22:04:54 +0900

    FIX; bugs
    logging error, return logic

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -7,14 +7,14 @@ import { getUserInfoWithDsmAuth, issuanceToken } from "./function/userAuthentica
 const provideToken: BusinessLogic = async (req, res, next) => {
   const token: string = req.headers["access-token"] as string;
   if(!token) {
-    next(new BadRequestError());
+    return next(new BadRequestError());
   }
   const userInfo = await getUserInfoWithDsmAuth(token);
   const authenticatedUser: User = await UserRepository.getQueryRepository()
   .findOrCreateUser(userInfo);
   const accessToken: string = await issuanceToken(authenticatedUser.user_id, "access");
   const refreshToken: string = await issuanceToken(authenticatedUser.user_id, "refresh");
-  res.status(200).json({
+  return res.status(200).json({
     "access-token": accessToken,
     "refresh-token": refreshToken,
   });
