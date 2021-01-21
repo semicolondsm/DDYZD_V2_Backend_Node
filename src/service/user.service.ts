6abc9eba48166a1a0b51cd01030de1eb81e7dcc8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-21 21:57:43 +0900

    CREATE; provide token service

---
+++ src/service/user.service.ts
@@ -0,0 +1,25 @@
+import { UserRepository } from "../entity/entity-repository/userReposiotry";
+import { User } from "../entity/model";
+import { BusinessLogic } from "../shared/BusinessLogicInterface";
+import { BadRequestError } from "../shared/exception";
+import { getUserInfoWithDsmAuth, issuanceToken } from "./function/userAuthentication";
+
+const provideToken: BusinessLogic = async (req, res, next) => {
+  const token: string = req.headers["access-token"] as string;
+  if(!token) {
+    next(new BadRequestError());
+  }
+  const userInfo = await getUserInfoWithDsmAuth(token);
+  const authenticatedUser: User = await UserRepository.getQueryRepository()
+  .findOrCreateUser(userInfo);
+  const accessToken: string = await issuanceToken(authenticatedUser.user_id, "access");
+  const refreshToken: string = await issuanceToken(authenticatedUser.user_id, "refresh");
+  res.status(200).json({
+    "access-token": accessToken,
+    "refresh-token": refreshToken,
+  });
+}
+
+export { 
+  provideToken,
+}
