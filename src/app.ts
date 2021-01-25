cf06d7a69a9e84501d2886c7c4ccf78766c73536
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-26 00:26:24 +0900

    FIX; static file path

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,8 @@ createConnection(createOptions).then((c) => {
 app.set("port", config.ServicePort || "3000");
 
 app.use(morgan("dev"));
-app.use("/file/feed", express.static(path.join(__dirname, "../../file/feed")));
-app.use("/file/club", express.static(path.join(__dirname, "../../file/club")));
+app.use("/file/feed", express.static(path.join(__dirname, "feeds")));
+app.use("/file/club", express.static(path.join(__dirname, "clubs")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req: Request, res: Response, next: NextFunction) => {
