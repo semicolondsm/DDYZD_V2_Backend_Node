cd5d96ac9cb59147f6567b91217ccfe389d0ff06
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-10 07:57:21 +0900

    UPDATE; user.service.ts
    캡슐화

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,72 +2,69 @@ import { ClubUserViewRepository } from "../entity/entity-repository/clubUserView
 import { UserRepository } from "../entity/entity-repository/userReposiotry";
 import { User } from "../entity/model";
 import { ClubUserView } from "../entity/view/ClubUserView";
-import { BusinessLogic } from "../shared/BusinessLogicInterface";
 import { ModifyUserInfoSchema } from "../shared/DataTransferObject";
 import { BadRequestError, UnAuthorizedTokenError } from "../shared/exception";
 import { getUserInfoWithDsmAuth, issuanceToken } from "./function/userAuthentication";
+import { ModifyUserInfoDto } from './../shared/DataTransferObject';
 
-const provideToken: BusinessLogic = async (req, res, next) => {
-  const token: string = req.headers["access-token"] as string;
-  if(!token) {
-    return next(new BadRequestError());
-  }
-  const userInfo = await getUserInfoWithDsmAuth(token);
-  const checkExistUser: User = await UserRepository.getQueryRepository().findUserByUniqueEmail(userInfo.email);
-  const authenticatedUser: User = checkExistUser ? 
-  checkExistUser : await UserRepository.getQueryRepository().createDefaultUser(userInfo);
-  return res.status(200).json({
-    "access_token": await issuanceToken(authenticatedUser.user_id, "access"),
-    "refresh_token": await issuanceToken(authenticatedUser.user_id, "refresh"),
-  });
-}
+export class UserService {
+  constructor(
+    private clubUserViewRepository: ClubUserViewRepository,
+    private userRepository: UserRepository,
+  ) {}
 
-const refreshToken: BusinessLogic = async (req, res, next) => {
-  const accessToken: string = await issuanceToken(+req.decoded.sub, "access");
-  res.status(200).json({
-    "access_token": accessToken,
-  });
-}
+  public async provideToken(token: string) {
+    if(!token) {
+      throw new BadRequestError();
+    }
+    const userInfo = await getUserInfoWithDsmAuth(token);
+    const checkExistUser: User = await this.userRepository.findUserByUniqueEmail(userInfo.email);
+    const authenticatedUser: User = checkExistUser ? 
+    checkExistUser : await this.userRepository.createDefaultUser(userInfo);
+    return {
+      "access_token": await issuanceToken(authenticatedUser.user_id, "access"),
+      "refresh_token": await issuanceToken(authenticatedUser.user_id, "refresh"),
+    };
+  }
 
-const showUserInfo: BusinessLogic = async (req, res, next) => {
-  const user: User = await UserRepository.getQueryRepository().findUserByClassIdentity(req.params.user_gcn);
-  const clubs: ClubUserView[] = await ClubUserViewRepository.getQueryRepository().findUsersClub(req.params.user_gcn);
-  if(!user) {
-    return next(new BadRequestError());
+  public async refreshToken(user_id: number) {
+    const accessToken: string = await issuanceToken(user_id, "access");
+    return {
+      "access_token": accessToken,
+    };
   }
-  delete user.device_token;
-  res.status(200).json({ ... user, clubs, });
-}
-
-const modifyUserInfo: BusinessLogic = async (req, res, next) => {
-  const { error, value } = ModifyUserInfoSchema.validate(req.body);
-  if(error) {
-    return next(new BadRequestError());
+  
+  public async showUserInfo(gcn: string) {
+    const user: User = await this.userRepository.findUserByClassIdentity(gcn);
+    const clubs: ClubUserView[] = await this.clubUserViewRepository.findUsersClub(gcn);
+    if(!user) {
+      throw new BadRequestError();
+    }
+    delete user.device_token;
+    return { ... user, clubs, };
   }
-  const modifiedUser: User = await UserRepository.getQueryRepository().putUserData(+req.decoded.sub, value);
-  if(!modifiedUser) {
-    return next(new UnAuthorizedTokenError());
+  
+  public async modifyUserInfo(data: ModifyUserInfoDto, user_id: number) {
+    const { error, value } = ModifyUserInfoSchema.validate(data);
+    if(error) {
+      throw new BadRequestError();
+    }
+    const modifiedUser: User = await this.userRepository.putUserData(user_id, value);
+    if(!modifiedUser) {
+      throw new UnAuthorizedTokenError();
+    }
+    return { msg: "Profile modify success" };
   }
-  res.status(200).json({ msg: "Profile modify success" });
-}
-
-const deviceToken: BusinessLogic = async (req, res, next) => {
-  const token: string = req.get("device-token");
-  if(!token || typeof token !== "string") {
-    return next(new BadRequestError());
+  
+  public async deviceToken(token: string, user_id: number) {
+    if(!token || typeof token !== "string") {
+      throw new BadRequestError();
+    }
+    const splitToken = token.split(" ");
+    if(splitToken[0] !== "Bearer") {
+      throw new UnAuthorizedTokenError();
+    }
+    await this.userRepository.deviceToken(user_id, splitToken[1]);
+    return { message: "Device token inserted" };
   }
-  const splitToken = token.split(" ");
-  if(splitToken[0] !== "Bearer") {
-    return next(new UnAuthorizedTokenError());
-  }
-  await UserRepository.getQueryRepository().deviceToken(+req.decoded.sub, splitToken[1]);
-  res.status(200).json({ message: "Device token inserted" });
-}
-
-export { 
-  provideToken,
-  refreshToken,
-  showUserInfo,
-  modifyUserInfo,
-  deviceToken,
 }
