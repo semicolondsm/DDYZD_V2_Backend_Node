e8109fd01af128a32f0586cfb8457e457535f900
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-10 00:59:48 +0900

    CREATE; club follow controller

--- src/router/club.controller.ts
+++ src/router/club.controller.ts
@@ -3,13 +3,16 @@ import { errorHandler } from "../middleware/errorHandler";
 import { BusinessLogic } from "../shared/BusinessLogicInterface";
 import * as clubService from "../service/club.service";
 import { validationNumberParameter } from "../middleware/validationParameter";
+import { verifyTokenMiddleware } from "../middleware/verifyToken";
 
 const clubServiceRotue: Router = Router();
 
 const showClubListHandler: BusinessLogic = errorHandler(clubService.showClubList);
 const showClubInfoHandler: BusinessLogic = errorHandler(clubService.showClubInfo);
+const followClubHandler: BusinessLogic = errorHandler(clubService.followClubHandler);
 
 clubServiceRotue.get("/list", showClubListHandler);
 clubServiceRotue.get("/:club_id/info", validationNumberParameter("club_id"), showClubInfoHandler);
+clubServiceRotue.post("/:club_id/follow", verifyTokenMiddleware, validationNumberParameter("club_id"), followClubHandler);
 
 export { clubServiceRotue }
