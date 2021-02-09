292392d4d7f651125d87298b740637abfcb76ca2
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-10 01:08:29 +0900

    FIX; router index.ts

--- src/router/club.controller.ts
+++ src/router/club.controller.ts
@@ -5,14 +5,14 @@ import * as clubService from "../service/club.service";
 import { validationNumberParameter } from "../middleware/validationParameter";
 import { verifyTokenMiddleware } from "../middleware/verifyToken";
 
-const clubServiceRotue: Router = Router();
+const clubServiceRouter: Router = Router();
 
 const showClubListHandler: BusinessLogic = errorHandler(clubService.showClubList);
 const showClubInfoHandler: BusinessLogic = errorHandler(clubService.showClubInfo);
 const followClubHandler: BusinessLogic = errorHandler(clubService.followClubHandler);
 
-clubServiceRotue.get("/list", showClubListHandler);
-clubServiceRotue.get("/:club_id/info", validationNumberParameter("club_id"), showClubInfoHandler);
-clubServiceRotue.post("/:club_id/follow", verifyTokenMiddleware, validationNumberParameter("club_id"), followClubHandler);
+clubServiceRouter.get("/list", showClubListHandler);
+clubServiceRouter.get("/:club_id/info", validationNumberParameter("club_id"), showClubInfoHandler);
+clubServiceRouter.post("/:club_id/follow", verifyTokenMiddleware, validationNumberParameter("club_id"), followClubHandler);
 
-export { clubServiceRotue }
+export { clubServiceRouter }
