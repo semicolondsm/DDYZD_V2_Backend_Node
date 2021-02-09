e7362443ac7d3d26d584de9b63ff52a5e30f163a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-10 07:18:48 +0900

    CREATE; club.router.ts
    요청을 처리해 controller로 넘기는 router 추가

---
+++ src/router/club.router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { errorHandler } from "../middleware/errorHandler";
+import { validationNumberParameter } from "../middleware/validationParameter";
+import { verifyTokenMiddleware } from "../middleware/verifyToken";
+import { ClubController } from "../controller/club.controller";
+
+const router: Router = Router();
+export const clubServiceRouter = (app: Router) => {
+  const clubController: ClubController = new ClubController();
+
+  app.use("/club", router);
+
+  router.get(
+    "/list", 
+    errorHandler(clubController.showClubList)
+  );
+
+  router.get(
+    "/:club_id/info", 
+    validationNumberParameter("club_id"), 
+    errorHandler(clubController.showClubInfo)
+  );
+
+  router.post(
+    "/:club_id/follow", 
+    verifyTokenMiddleware, 
+    validationNumberParameter("club_id"), 
+    errorHandler(clubController.followClubHandler)
+  );
+}
