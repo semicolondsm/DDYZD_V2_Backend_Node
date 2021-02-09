d70ae867bc3b7256ae8254d0e2a76f4ac585c9b0
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-10 07:21:18 +0900

    UPDATE; app.ts
    코드 분할, 에러 처리 추가

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,15 @@
 import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
 import path from "path";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import cors from "cors";
-import { createConnection } from "typeorm";
-import { createOptions } from './ormconfig';
-import { config } from "./config";
-import { HttpError, NotFoundError } from "./shared/exception";
 import { logger } from "./shared/logger";
-
-import ddyzdRouter from "./router";
+import { initApplication } from "./loader";
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
-const app: express.Application = express();
+initApplication();
 
-createConnection(createOptions).then((c) => {
-  console.log("Database Connect Success");
-}).catch(err => {
+process.on("uncaughtException", (err: Error) => {
   console.error(err);
-  process.exit(1);
-});
-
-app.set("port", config.ServicePort || "3000");
-
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
-
-app.use("/", ddyzdRouter);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError(req.url));  
-});
-
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  logger.info(err.message);
-  const statusCode: number = err.statusCode || 500;
-  res.status(statusCode)
-  .json({
-    statusCode: statusCode,
-    message: err.message,
-    timeStamp: new Date(),
-  });
-});
-
-app.listen(app.get("port"), () => {
-  console.log("server on", app.get("port"));
+  logger.error("uncaughtException");
+  logger.error(err);
 });
