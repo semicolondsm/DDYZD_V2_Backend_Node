c109e418012dba5ea230d4e72ed72cf643a26d77
sonchaegeon <thscorjs1002@naver.com>
2021-02-10 17:57:37 +0900

    UPDATE; unfollow club service

--- src/service/club.service.ts
+++ src/service/club.service.ts
@@ -1,7 +1,7 @@
 import { ClubFollowRepository } from "../entity/entity-repository/clubFollowRepository";
 import { ClubRepository } from "../entity/entity-repository/clubRepository";
 import { UserRepository } from "../entity/entity-repository/userReposiotry";
-import { Club, User } from "../entity/model";
+import { Club, ClubFollow, User } from "../entity/model";
 import { BusinessLogic } from "../shared/BusinessLogicInterface";
 import { ClubListResObj } from "../shared/DataTransferObject";
 import { BadRequestError } from "../shared/exception";
@@ -16,11 +16,15 @@ const showClubList: BusinessLogic = async  (req, res, next) => {
   res.status(200).json(clubs);
 }
 
-const followClubHandler: BusinessLogic = async (req, res, next) => {
+const followClub: BusinessLogic = async (req, res, next) => {
   const userRecord: User = await UserRepository.getQueryRepository().findOne(+req.decoded.sub);
   const clubRecord: Club = await ClubRepository.getQueryRepository().findOne(+req.params.club_id);
+  const existRecord: ClubFollow = await ClubFollowRepository.getQueryRepository().findOne({
+    user: userRecord,
+    club: clubRecord
+  });
 
-  if(!userRecord || !clubRecord) {
+  if(!userRecord || !clubRecord || existRecord) {
     return next(new BadRequestError());
   }
 
@@ -28,7 +32,24 @@ const followClubHandler: BusinessLogic = async (req, res, next) => {
   res.status(200).json({ message: "User following club now" })
 }
 
+const unfollowClub: BusinessLogic = async (req, res, next) => {
+  const userRecord: User = await UserRepository.getQueryRepository().findOne(+req.decoded.sub);
+  const clubRecord: Club = await ClubRepository.getQueryRepository().findOne(+req.params.club_id);
+  const existRecord: ClubFollow = await ClubFollowRepository.getQueryRepository().findOne({
+    user: userRecord,
+    club: clubRecord
+  });
+
+  if(!userRecord || !clubRecord || !existRecord) {
+    return next(new BadRequestError());
+  }
+
+  await ClubFollowRepository.getQueryRepository().deleteClubFollow(userRecord, clubRecord);
+  res.status(200).json({ message: "User unfollowing club now" });
+}
+
 export { 
   showClubList,
-  followClubHandler,
+  followClub,
+  unfollowClub
 }
