a0ff9406cd47db3be5fcea7c631d1cbd93aded11
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-11 20:01:00 +0900

    DELETE; generation view

--- src/entity/view/generation-view/query/asClubTagView.sql
+++
@@ -1,8 +0,0 @@
-SELECT club.club_id AS id, 
-club.club_name AS name, 
-club.description AS description,
-club.profile_image AS club_image,
-tag.title AS tag_name
-FROM club 
-LEFT JOIN club_has_tag ON club.club_id = club_has_tag.club_id 
-LEFT JOIN tag ON club_has_tag.tag_id = tag.id
