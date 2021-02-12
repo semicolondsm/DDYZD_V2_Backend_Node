930154eebeb8ff5f06c0b20267206c65660e82a4
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-12 20:20:42 +0900

    UPDATE; club user view repository
    add find clubs member method

--- src/entity/entity-repository/clubUserViewRepository.ts
+++ src/entity/entity-repository/clubUserViewRepository.ts
@@ -1,4 +1,5 @@
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { ClubMemberResObj } from "../../shared/DataTransferObject";
 import { ClubUserView } from "../view";
 
 @EntityRepository(ClubUserView)
@@ -13,6 +14,16 @@ export class ClubUserViewRepository extends Repository<ClubUserView> {
     .addSelect("view.club_id")
     .where("view.gcn = :gcn", { gcn })
     .andWhere("view.result = 1")
+    .getMany();
+  }
+
+  public findClubsMember(club_id: number): Promise<ClubMemberResObj[]> {
+    return this.createQueryBuilder("view")
+    .select("view.user_name")
+    .addSelect("view.profile_image")
+    .addSelect("view.git")
+    .addSelect("view.gcn")
+    .where("view.club_id = :club_id", { club_id })
     .getMany();
   }
 }
