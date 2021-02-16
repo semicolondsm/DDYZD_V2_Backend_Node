4493eb73050b2332bcbf7cffe026ec036486a91a
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-17 02:30:36 +0900

    UPDATE; add validation request middeware

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -3,7 +3,7 @@ import { errorHandler } from "../middleware/errorHandler";
 import { verifyRefreshTokenMiddleware, verifyTokenMiddleware } from "../middleware/verifyToken";
 import { UserController } from "../controller/user.controller";
 import { validationRequest } from "../middleware/validatoinRequest";
-import { ModifyUserInfoSchema } from "../shared/DataTransferObject";
+import { ModifyUserInfoSchema, ProvideUserTokenSchema } from "../shared/DataTransferObject";
 
 const router: Router = Router();
 export const userServiceRouter = (app :Router) => {
@@ -18,6 +18,7 @@ export const userServiceRouter = (app :Router) => {
 
   router.post(
     "/token/code",
+    validationRequest(ProvideUserTokenSchema),
     errorHandler(userController.proviceTokenWithCode)
   );
   
