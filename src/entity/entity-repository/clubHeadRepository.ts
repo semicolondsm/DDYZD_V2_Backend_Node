0c233c528d8bb8057cb44124618ea3ee34499274
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-18 23:51:54 +0900

    UPDATE; club head repostiory
    add find club head method

--- src/entity/entity-repository/clubHeadRepository.ts
+++ src/entity/entity-repository/clubHeadRepository.ts
@@ -1,9 +1,23 @@
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { ClubMemberResObj } from "../../shared/DataTransferObject";
 import { ClubHead } from "../model";
 
 @EntityRepository(ClubHead) 
 export class ClubHeadRepository extends Repository<ClubHead> {
   static getQueryRepository() {
     return getCustomRepository(ClubHeadRepository);
+  }
+
+  public findClubHead(club_id: number): Promise<ClubMemberResObj> {
+    return this.createQueryBuilder("head")
+    .select("user.user_id", "user_id")
+    .addSelect("user.name", "user_name")
+    .addSelect("user.image_path", "profile_image")
+    .addSelect("user.gcn", "gcn")
+    .addSelect("user.github_url")
+    .leftJoin("head.user", "user")
+    .leftJoin("head.club", "club")
+    .where("club.club_id = :club_id", { club_id })
+    .getRawOne();
   }
 }
