5dc98824eaf97db1e5d216462e42855d6c951d91
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-02-21 02:47:46 +0900

    UPDATE; club repository
    remvoe find info by id method

--- src/entity/entity-repository/clubRepository.ts
+++ src/entity/entity-repository/clubRepository.ts
@@ -8,17 +8,6 @@ export class ClubRepository extends Repository<Club> {
     return getCustomRepository(ClubRepository);
   }
 
-  public findInfoById(club_id: number): Promise<ClubInfoResObj> {
-    return this.createQueryBuilder("club")
-    .select("club.club_id", "clubid")
-    .addSelect("club.club_name", "clubname")
-    .addSelect("club.profile_image", "clubimage")
-    .addSelect("club.banner_image", "backimage")
-    .addSelect("club.description", "description")
-    .where("club.club_id = :id", { id: club_id })
-    .getRawOne();
-  }
-
   public findClubRecruitments(club_id: number): Promise<Club> {
     return this.createQueryBuilder("club")
     .select("club.close_at")
