bac904fd1fe61820e49c1a997c3ef92812214b9b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-01 22:08:39 +0900

    UPDATE; club has tag model
    변경사항 적용

--- src/entity/model/ClubHasTag.ts
+++ src/entity/model/ClubHasTag.ts
@@ -1,12 +1,10 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, JoinColumn, ManyToOne } from "typeorm";
 import { Club } from "./Club";
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
 import { Tag } from "./Tag";
 
 @Entity("club_has_tag")
-export class ClubHasTag {
-  @PrimaryGeneratedColumn()
-  clubtag_id: number;
-
+export class ClubHasTag extends EntityWithIdColumn {
   @ManyToOne(() => Club, club => club.clubHasTags)
   @JoinColumn({ name: "club_id" })
   club: Club;
