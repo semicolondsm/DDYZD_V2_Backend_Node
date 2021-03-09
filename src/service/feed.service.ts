262cb8697ef5d2e98a766c65ea7a727699b69fb8
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-09 15:26:58 +0900

    UPDATE; feed.service.ts
    add feed list

--- src/service/feed.service.ts
+++ src/service/feed.service.ts
@@ -1,21 +1,33 @@
 import { ClubFollowRepository } from "../entity/entity-repository/clubFollowRepository";
 import { ClubMemberRepository } from "../entity/entity-repository/clubMemberRepository";
+import { FeedFlagRepository } from "../entity/entity-repository/feedFlagRepository";
 import { FeedRepotisoty } from "../entity/entity-repository/feedRepository";
-import { FeedListDefaultResObj, FeedListResObj } from "../shared/DataTransferObject";
+import { Feed } from "../entity/model";
+import { FeedListResObj } from "../shared/DataTransferObject";
 
 export class FeedService {
   constructor(
     private feedRepository: FeedRepotisoty,
-    private clubFollowRepository: ClubFollowRepository,
-    private clubMemberRepository: ClubMemberRepository
+    private clubMemberRepository: ClubMemberRepository,
+    private feedFlagRepository: FeedFlagRepository,
+    private clubFollowRepository: ClubFollowRepository
   ) {} 
 
-  public async getFeedList(pageNumber: number, user_id?: number): Promise<FeedListResObj> {
-    const defaultResults: FeedListDefaultResObj[] = await this.feedRepository.getFeedList(pageNumber);
-    if(user_id) {
-
-    } else {
-
-    }
+  public async getFeedList(pageNumber: number, user_id?: number): Promise<FeedListResObj[]> {
+    const feeds: Feed[] = await this.feedRepository.getFeedList(pageNumber);
+    return await Promise.all(feeds.map(async (feed: Feed): Promise<FeedListResObj> => {
+      return {
+        feedId: feed.id,
+        clubName: feed.club.name,
+        profileImage: feed.club.profile_image,
+        uploadAt: feed.upload_at,
+        content: feed.content,
+        media: await this.feedRepository.getFeedMedia(feed.id),
+        flags: await this.feedRepository.getFeedFlags(feed.id),
+        owner: user_id ? await this.clubMemberRepository.checkIsClubMember(feed.club.id, user_id) : false,
+        flag: user_id ? await this.feedFlagRepository.checkIsFlaged(feed.id, user_id) : false,
+        follow: user_id ? await this.clubFollowRepository.checkIsFollowed(user_id, feed.club.id) : false
+      }
+    }));
   }
 }
