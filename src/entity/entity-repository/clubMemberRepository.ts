ed6485407706068d9c9866d51c6efb0cba8cf6c5
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-09 10:20:28 +0900

    UPDATE; club member repository
    add check club member method

--- src/entity/entity-repository/clubMemberRepository.ts
+++ src/entity/entity-repository/clubMemberRepository.ts
@@ -6,4 +6,12 @@ export class ClubMemberRepository extends Repository<ClubMember> {
   static getQueryRepository() {
     return getCustomRepository(ClubMemberRepository);
   }
+
+  public async checkIsClubMember(club_id: number, user_id: number): Promise<boolean> {
+    const member: ClubMember = await this.createQueryBuilder("member")
+    .where("member.club_id = :club_id", { club_id })
+    .andWhere("member.user_id = :user_id", { user_id })
+    .getOne();
+    return !!member;
+  }
 }
