90cd99c191305f0c1f75d3e1f6eaed808800fb76
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-03-30 19:27:03 +0900

    CREATE; Activity Detail Repository
    Add getUserActivities  Method

---
+++ src/entity/entity-repository/ActivityDetailRepository.ts
@@ -0,0 +1,22 @@
+import { EntityRepository, getCustomRepository, Repository } from "typeorm";
+import { ActivityDetails } from "../model";
+
+@EntityRepository(ActivityDetails) 
+export class ActivityDetailRepository extends Repository<ActivityDetails> {
+  static getQueryRepository() {
+    return getCustomRepository(ActivityDetailRepository);
+  }
+
+  public getUserActivities(user_id: number) {
+    return this.createQueryBuilder("ac")
+    .select("activity.activity", "activity")
+    .addSelect("club.name", "club_name")
+    .addSelect("club.profile_image", "club_image")
+    .addSelect("club.id", "club_id")
+    .leftJoin("ac.club", "club")
+    .leftJoin("ac.user", "user")
+    .leftJoin("ac.activity", "activity")
+    .where("user.id = :id", { id: user_id })
+    .getRawMany();
+  }
+}
