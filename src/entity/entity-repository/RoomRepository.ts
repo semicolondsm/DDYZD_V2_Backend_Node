271867b36dfe8b8fa366a0f80016cb834549181f
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-04-16 22:44:54 +0900

    UPDATE; RoomRepository.ts change room status method

--- src/entity/entity-repository/RoomRepository.ts
+++ src/entity/entity-repository/RoomRepository.ts
@@ -1,10 +1,21 @@
 import { EntityRepository, getCustomRepository, Repository } from "typeorm";
-import { Club } from "../model";
 import { Room } from "../model/Room";
 
 @EntityRepository(Room) 
 export class RoomRepository extends Repository<Room> {
   static getQueryRepository() {
     return getCustomRepository(RoomRepository);
+  }
+  
+  public async changeRoomStatus(room: Room) {
+    await this.queryRunner.startTransaction();
+    try {
+      await this.queryRunner.manager.save(room);
+      await this.queryRunner.commitTransaction();
+    } catch(err) {
+      await this.queryRunner.rollbackTransaction();
+    } finally {
+      await this.queryRunner.release();
+    }
   }
 }
