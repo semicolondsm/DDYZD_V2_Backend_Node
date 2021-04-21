183edf70692338c8a3669f80017eb160d90eb305
parksangwoo <b01056721617@gmail.com>
2021-04-21 23:15:10 +0900

    CREATE; noticeModel

--- src/entity/model/Notice.ts
+++ src/entity/model/Notice.ts
@@ -1,1 +1,18 @@
-test
+import { Column, CreateDateColumn, Entity } from "typeorm";
+import { EntityWithIdColumn } from "./EntityWithPrimaryColumn";
+import { Writer } from "../../shared/checkAdmin";
+
+@Entity('notice')
+export class Notice extends EntityWithIdColumn{
+    @Column()
+    title: string;
+
+    @Column({ type: "varchar", length: 3000})
+    content: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column({ type: "enum", enum: Writer})
+    writer: Writer;
+}
