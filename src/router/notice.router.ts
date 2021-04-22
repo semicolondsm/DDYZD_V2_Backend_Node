38dc175960f6dc0b359edd342615d481a4a36a28
parksangwoo <b01056721617@gmail.com>
2021-04-22 15:20:20 +0900

    CREATE; noticeRouter

---
+++ src/router/notice.router.ts
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import { errorHandler } from "../middleware/errorHandler";
+import { validationNumberParameter } from "../middleware/validationParameter";
+import { verifyTokenMiddleware } from "../middleware/verifyToken";
+import { NoticeController } from "../controller/notice.controller";
+import { verifyTokenOrDone } from "../middleware/verifyTokenOrDone";
+
+const router: Router = Router();
+export const noticeServiceRouter = (app: Router) => {
+    const noticeController: NoticeController = new NoticeController();
+
+    app.use("/notice", router);
+
+    router.get(
+        "/check",
+        errorHandler(noticeController.checkIsAdmin)
+    );
+
+    router.post(
+        "/",
+        verifyTokenMiddleware,
+        errorHandler(noticeController.createNotice)
+    );
+
+    router.get(
+        "/?size=4&page=1",
+        errorHandler(noticeController.getAllNotice)
+    );
+
+    router.get(
+        "/:notice_id",
+        errorHandler(noticeController.getSpecificNotice)
+    );
+
+    router.patch(
+        "/:notice_id",
+        verifyTokenMiddleware,
+        errorHandler(noticeController.updateNotice)
+    );
+
+    router.delete(
+        "/:notice_id",
+        verifyTokenMiddleware,
+        errorHandler(noticeController.deleteNotice)
+    );
+}
