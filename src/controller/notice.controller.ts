99bdc7416efe149d82c522da7ed4dcf108432c68
parksangwoo <b01056721617@gmail.com>
2021-04-22 15:19:59 +0900

    CREATE; noticeController

--- src/controller/notice.controller.ts
+++ src/controller/notice.controller.ts
@@ -1,13 +1,23 @@
 import { NoticeRepository } from "../entity/entity-repository/noticeRepository";
+import { ClubMemberRepository } from "../entity/entity-repository/clubMemberRepository";
 import { NoticeService } from "../service/notice.service";
 import { Notice } from "../entity/model/Notice";
 import { BusinessLogic } from "../shared/BusinessLogicInterface";
 
-export class FeedController {
+export class NoticeController {
     private noticeService: NoticeService = new NoticeService(
-        NoticeRepository.getQueryRepository()
+        NoticeRepository.getQueryRepository(),
+        ClubMemberRepository.getQueryRepository()
     );
 
+    public checkIsAdmin: BusinessLogic = async (req, res, next) => {
+        const clubId: number = 32;
+        await this.noticeService.checkIsAdmin(clubId, +req.decoded.sub);
+        res.status(200).json({
+            message: "pass!!"
+        })
+    }
+    
     public getAllNotice: BusinessLogic = async (req, res, next) => {
         const notices: Notice[] = await this.noticeService.getAllNotice(+req.query.size, +req.query.page);
         res.status(200).json(notices);
@@ -25,11 +35,11 @@ export class FeedController {
 
     public updateNotice: BusinessLogic = async (req, res, next) => {
         await this.noticeService.updateNotice(+req.params.notice_id, req.writer, req.title, req.content);
-        res.status(200).json("success");
+        res.status(200).json("notice patched");
     }
 
     public deleteNotice: BusinessLogic = async (req, res, next) => {
         await this.noticeService.deleteNotice(+req.params.notice_id);
-        res.status(200).json("success");
+        res.status(200).json("notice deleted");
     }
 }
