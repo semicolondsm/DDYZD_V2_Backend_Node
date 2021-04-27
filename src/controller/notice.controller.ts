c5139216fcfe41c4d37b329f505b589daebc7bf4
parksangwoo <b01056721617@gmail.com>
2021-04-27 22:29:26 +0900

    UPDATE; package.json

--- src/controller/notice.controller.ts
+++ src/controller/notice.controller.ts
@@ -10,9 +10,10 @@ export class NoticeController {
         ClubMemberRepository.getQueryRepository()
     );
 
+    private clubId: number = 2;
+
     public checkIsAdmin: BusinessLogic = async (req, res, next) => {
-        const clubId: number = 32;
-        await this.noticeService.checkIsAdmin(clubId, +req.decoded.sub);
+        await this.noticeService.checkIsNotAdmin(this.clubId, +req.decoded.sub);
         res.status(200).json({
             message: "pass!!"
         })
@@ -20,26 +21,38 @@ export class NoticeController {
     
     public getAllNotice: BusinessLogic = async (req, res, next) => {
         const notices: Notice[] = await this.noticeService.getAllNotice(+req.query.size, +req.query.page);
-        res.status(200).json(notices);
+        if(!notices) {
+            res.status(200).json({
+                message: "공지사항이 없습니다."
+            })
+        } else {
+            res.status(200).json(notices);
+        }
     }
     
     public getSpecificNotice: BusinessLogic = async (req, res, next) => {
         const notice: Notice = await this.noticeService.getSpecificNotice(+req.params.notice_id);
-        res.status(200).json(notice);
+        if(!notice) {
+            res.status(200).json({
+                message: "공지사항이 없습니다."
+            })
+        } else {
+            res.status(200).json(notice);
+        }
     } 
 
     public createNotice: BusinessLogic = async (req, res, next) => {
-        await this.noticeService.createNotice(req.writer, req.title, req.content);
+        await this.noticeService.createNotice(req.body, this.clubId, +req.decoded.sub);
         res.status(200).json("success");
     }
 
     public updateNotice: BusinessLogic = async (req, res, next) => {
-        await this.noticeService.updateNotice(+req.params.notice_id, req.writer, req.title, req.content);
+        await this.noticeService.updateNotice(+req.params.notice_id, this.clubId, +req.decoded.sub, req.body);
         res.status(200).json("notice patched");
     }
 
     public deleteNotice: BusinessLogic = async (req, res, next) => {
-        await this.noticeService.deleteNotice(+req.params.notice_id);
+        await this.noticeService.deleteNotice(+req.params.notice_id, this.clubId, +req.decoded.sub);
         res.status(200).json("notice deleted");
     }
 }
