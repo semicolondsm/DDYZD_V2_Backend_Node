9597b21bc6d9da8aec6b5712c2559f4ec404175e
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-06-19 20:53:37 +0900

    FIX; 버전 호환 문제 수정

--- src/loader/loadExpress.ts
+++ src/loader/loadExpress.ts
@@ -1,16 +1,27 @@
 import fs from "fs";
-import { NextFunction, Request, Response , Application } from "express";
-import bodyParser from "body-parser";
+import express, { NextFunction, Request, Response , Application } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { config } from "../config";
 import { HttpError, NotFoundError } from "../shared/exception";
 import { ddyzdRouter } from "../router";
 import { logger } from "../shared/logger";
+import { ServerResponse } from "http";
 
 export const loadExpress = (app: Application) => {
   app.set("port", config.ServicePort || "3000");
-  
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    morgan("combined", {
+      stream: fs.createWriteStream("./logs/log.log", { encoding: "utf-8" })
+    })(req, res as ServerResponse, next);
+  });
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    express.json()(req, res as ServerResponse, next);
+    express.urlencoded({ extended: false })(req, res as ServerResponse, next);
+  });
+
   app.use(cors());
 
   app.use("/", ddyzdRouter());
